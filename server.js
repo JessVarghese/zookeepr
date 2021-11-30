c397bb529c734e4e5d7209f09dcd682e844e8aa8
Jess Varghese <jessvarghese@gmail.com>
2021-11-30 10:54:42 -0500

    added data validation

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const { query } = require('express');
-const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
+
+const express = require('express');
+const { animals } = require("./data/animals.json")
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const { animals } = require("./data/animals.json")
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 
 function filterByQuery(query, animalsArray) {
@@ -53,6 +59,34 @@ function filterByQuery(query, animalsArray) {
     return result;
   }
 
+  function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+      path.join(__dirname, './data/animals.json'),
+      JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    //our function's main code will go here!
+
+    //return finished code to post route for response
+    return animal;
+  }
+
+  function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
   
   app.get('/', (req, res) => {
        let results = animals;
@@ -79,6 +113,22 @@ if (result) {
 }
 
 });
+
+
+app.post('/api/animals', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
+});
+
+
 
 
 
